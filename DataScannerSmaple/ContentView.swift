649b1cffdd5f502bfd251ee5267220119c7a2c96
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-01-16 15:32:22 +0100

    Initial Commit

---
+++ DataScannerSmaple/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DataScannerSmaple
+//
+//  Created by Vladyslav Dikhtiaruk on 16/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DataScannerSmaple/DataScannerSmapleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataScannerSmapleApp.swift
+//  DataScannerSmaple
+//
+//  Created by Vladyslav Dikhtiaruk on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct DataScannerSmapleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
